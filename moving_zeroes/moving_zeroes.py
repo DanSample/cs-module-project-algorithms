69c686a57a051fb172a90e72e8ad644b06ba6658
Dan Sample <danssample@gmail.com>
2020-06-11 21:11:35 -0700

    added comments to moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,20 +5,23 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # takes an arr of ints and moves each NON-ZERO
     ## int to the left side
-    # loop over arr
+    # init 2 variables 1 at the start and 1 at the end
     left = 0
     right = len(arr) - 1
+    # while the left is left of right
     while left <= right:
+        #if the left is 0 and the right is not 0
         if arr[left] == 0 and arr[right] != 0:
+            #swap
             arr[left], arr[right] = arr[right], arr[left]
         else:
+            # if left not 0, increment left
             if arr[left] != 0:
                 left+=1
+            # if right is 0, decrement right
             if arr[right] == 0:
                 right -= 1
     return arr
-    # return the altered arr
-    # the order does not matter 
 
 
 if __name__ == '__main__':
