bb298ebe9150deadd93b46e4ec1f34db79401396
Dan Sample <danssample@gmail.com>
2020-06-11 18:44:51 -0700

    completed first-pass single number

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,7 +3,19 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+    # takes in an integer
+    # cookies can be eaten 1, 2, or 3 at a time
+    # deal with base case(s):
+    ## if n is 0 or negative return 0
+    if n <= 0:
+        return 0
+    ## if there is only 1 cookie return 1
+    elif n == 1:
+        return 1    
+    # deal with case: 2 cookies
+    # deal with case: 3 cookies
+    # deal with case: 3+ cookies
+    # return the number of ways all the cookies can be eaten
 
     pass
 
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,15 +1,24 @@
+from collections import Counter
 '''
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    # Takes in a non-empty array
+    # create a temp dict with Counter, this will count all the
+    ## elements and assign them as keys and the values as the 
+    ### the amount of times it appears in the arr  
+    temp = Counter(arr) 
+    # loop through the dict and check to see which value == 1
+    for value in temp:
+        if temp[value] == 1:
+            # return the number
+            return value
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    single_number(arr)
 
     print(f"The odd-number-out is {single_number(arr)}")
