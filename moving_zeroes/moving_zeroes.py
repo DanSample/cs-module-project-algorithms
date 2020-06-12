08501aa99d2063653ea4270da5e3d0654aa5c0df
Dan Sample <danssample@gmail.com>
2020-06-11 20:40:43 -0700

    finishing moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,13 +3,27 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    # takes an arr of ints and moves each NON-ZERO
+    ## int to the left side
+    # loop over arr
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        if arr[left] == 0 and arr[right] != 0:
+            arr[left], arr[right] = arr[right], arr[left]
+        else:
+            if arr[left] != 0:
+                left+=1
+            if arr[right] == 0:
+                right -= 1
+    return arr
+    # return the altered arr
+    # the order does not matter 
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
+    moving_zeroes(arr)
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
